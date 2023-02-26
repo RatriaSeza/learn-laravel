d3374ae42eeb1c7addef063150fceae775c67b06
RatriaSeza <satriareza2002@gmail.com>
2023-02-26 23:57:47 +0700

    13. Searching & Pagination

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home', [
-        "title" => "Home"
+        "title" => "Home",
+        'active' => "home",
     ]);
 });
 
@@ -27,6 +28,7 @@ Route::get('/about', function () {
     return view('about', [
         "title" => "About",
         "name" => "Ratria Seza",
+        'active' => "about",
         "email" => "ratriaseza@gmail.com",
         "img" => "Lupi.jpeg"
     ]);
@@ -41,21 +43,5 @@ Route::get('/categories', function(){
         'title' => 'Post Categories',
         'active' => "categories",
         'categories' => Category::all()
-    ]);
-});
-
-Route::get('/categories/{category:slug}', function(Category $category){
-    return view('posts', [
-        'title' => "Post By Category : $category->name",
-        'active' => "categories",
-        'posts' => $category->posts->load('category', 'author')
-    ]);
-});
-
-Route::get('/authors/{author:username}', function(User $author){
-    return view('posts', [
-        'title' => "Post By Author : $author->name",
-        'active' => "about",
-        'posts' => $author->posts->load('category', 'author')
     ]);
 });
